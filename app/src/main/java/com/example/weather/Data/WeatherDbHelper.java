feb1ff03b02aa2158d5984f5e6bd5221ec59367e
Nihar Patel <Niharmpatel@gmail.com>
2020-06-02 23:40:34 -0400

    db

---
+++ app/src/main/java/com/example/weather/Data/WeatherDbHelper.java
@@ -0,0 +1,76 @@
+package com.example.weather.Data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class WeatherDbHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "weather.db";
+    private static final int DATABASE_VERSION = 1;
+    public WeatherDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    /**
+     * Called when the database is created for the first time. This is where the
+     * creation of tables and the initial population of the tables should happen.
+     *
+     * @param sqLiteDatabase The database.
+     */
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+
+        final String SQL_CREATE_WEATHER_TABLE =
+
+                "CREATE TABLE " + WeatherContract.WeatherEntry.TABLE_NAME + " (" +
+
+                        /*
+                         * WeatherEntry did not explicitly declare a column called "_ID". However,
+                         * WeatherEntry implements the interface, "BaseColumns", which does have a field
+                         * named "_ID". We use that here to designate our table's primary key.
+                         */
+                        WeatherContract.WeatherEntry._ID               + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+
+                        WeatherContract.WeatherEntry.COLUMN_DATE       + " INTEGER, "                 +
+
+                        WeatherContract.WeatherEntry.COLUMN_WEATHER_ID + " INTEGER, "                 +
+
+                        WeatherContract.WeatherEntry.COLUMN_MIN_TEMP   + " REAL, "                    +
+                        WeatherContract.WeatherEntry.COLUMN_MAX_TEMP   + " REAL, "                    +
+
+                        WeatherContract.WeatherEntry.COLUMN_HUMIDITY   + " REAL, "                    +
+                        WeatherContract.WeatherEntry.COLUMN_PRESSURE   + " REAL, "                    +
+
+                        WeatherContract.WeatherEntry.COLUMN_WIND_SPEED + " REAL, "                    +
+                        WeatherContract.WeatherEntry.COLUMN_DEGREES    + " REAL" + ");";
+
+        sqLiteDatabase.execSQL(SQL_CREATE_WEATHER_TABLE);
+
+    }
+
+    /**
+     * Called when the database needs to be upgraded. The implementation
+     * should use this method to drop tables, add tables, or do anything else it
+     * needs to upgrade to the new schema version.
+     *
+     * <p>
+     * The SQLite ALTER TABLE documentation can be found
+     * <a href="http://sqlite.org/lang_altertable.html">here</a>. If you add new columns
+     * you can use ALTER TABLE to insert them into a live table. If you rename or remove columns
+     * you can use ALTER TABLE to rename the old table, then create the new table and then
+     * populate the new table with the contents of the old table.
+     * </p><p>
+     * This method executes within a transaction.  If an exception is thrown, all changes
+     * will automatically be rolled back.
+     * </p>
+     *
+     * @param sqLiteDatabase         The database.
+     * @param oldVersion The old database version.
+     * @param newVersion The new database version.
+     */
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+
+    }
+}
